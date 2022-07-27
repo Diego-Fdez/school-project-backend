df0512c837dedb79fda1bf4ce199cf4043a55df1
Diego-Fdez <dieghoxcr7@gmail.com>
2022-07-26 23:28:25 -0600

    quito el body-parser

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ import sectionRoute from './routes/sectionRoute.js';
 const app = express();
 
 //Middleware
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+// app.use(bodyParser.json({ limit: '30mb', extended: true }));
+// app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI, {
