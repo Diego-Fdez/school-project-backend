46c3a1fe90f1de2d4de93a1374f06246b26510bc
Diego-Fdez <dieghoxcr7@gmail.com>
2023-02-20 16:29:46 -0600

    quito cors options

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import cors from 'cors';
+
 import app from './app.js';
 import { DbConnect } from './database/dbConnection.js';
 
@@ -9,23 +9,6 @@ const PORT = process.env.PORT || 5000;
 
 /* Connecting to the database. */
 DbConnect();
-
-//cors configuration
-const whitelist = [process.env.FRONTEND_URL];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.includes(origin)) {
-      //You can refer to the API
-      callback(null, true);
-    } else {
-      //It's not allowed
-      callback(new Error('Error de Cors'));
-    }
-  },
-};
-
-app.use(cors(corsOptions));
 
 /* Listening to the port. */
 app.listen(PORT, () => {
